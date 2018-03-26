4ce35e288d05f9c6c291a5ed0b0ec5c17f9f6c04
3I_10K <eliocamison@gmail.com>
2018-03-26 17:54:48 +0200

    Show table CLIENT;

--- src/com/esliceu/Main.java
+++ src/com/esliceu/Main.java
@@ -4,6 +4,12 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
+import org.gjt.mm.mysql.Driver;
 
 public class Main {
 
@@ -15,8 +21,22 @@ public class Main {
             while (true) {
                 Socket socket = listener.accept();
                 try {
-                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+                    Class.forName("com.mysql.jdbc.Driver");
+                    // Establecemos la conexión con la base de datos.
+                    Connection conexion = DriverManager.getConnection ("jdbc:mysql://localhost/empresa","root", "test");
+                    // Preparamos la consulta
+                    Statement s = conexion.createStatement();
+                    ResultSet rs = s.executeQuery ("select * from CLIENT");
                     StringBuilder response = new StringBuilder();
+                    // Recorremos el resultado, mientras haya registros para leer, y escribimos el resultado en pantalla.
+                    while (rs.next())
+                    {
+                        response.append (rs.getInt (1) + " " + rs.getString (2)+ " " + rs.getString(3));
+                        //response.append("<html><body><h1>Hello!</h1></body></html>");
+                    }
+
+                    PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
                     response.append("<html><body><h1>Hello!</h1></body></html>");
 
 
@@ -27,9 +47,11 @@ public class Main {
                     out.println(response);
                     out.flush();
                     out.close();
-
-
+                    // Cerramos la conexion a la base de datos.
+                    conexion.close();
                  
+                } catch (Exception e) {
+                    e.printStackTrace();
                 } finally {
                     socket.close();
                 }
